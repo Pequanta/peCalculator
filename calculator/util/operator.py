f0c749facc91856cb0bb2f3efc7fede614b37d35
PenielY <96361630+Pequanta@users.noreply.github.com>
2022-11-26 21:45:53 +0300

    Update operator.py
    In this commit I increased the amount of entries for the basic operations

--- calculator/util/operator.py
+++ calculator/util/operator.py
@@ -1,5 +1,5 @@
 import math
-
+_tool = []
 def handler(self,operation,event = None):
     self.operation = operation
     simple = ['mult','div','sum','diff','^']
@@ -7,13 +7,13 @@ def handler(self,operation,event = None):
     self.act = str()
     if operation in simple:
         self.n_entry.get()
-        self.first_num = self.n_entry.get()
+        self.calc_num = self.n_entry.get()
         self.n_entry.delete(0,"end")
         self.act = str(operation)
-        self.tool = []
-        self.tool.append(self.act)
-        self.tool.append(self.first_num)
-        return self.tool
+        if operation not in _tool:
+        	_tool.append(self.act)
+        _tool.append(self.calc_num)
+    return _tool
 
     elif operation in unary:
         self.n_entry.get()
@@ -32,29 +32,37 @@ def handler(self,operation,event = None):
         else:
             return
     elif operation == "equal":
-        if self.tool[0] == "sum":
+        if _tool[0] == "sum":
             self.n_entry.get()
-            second_num = self.n_entry.get()
-            result = int(self.first_num) + int(second_num)
+            last_element = self.n_entry.get()
+            self.n_entry.delete(0, "end")
+            result = sum(list(map(float , _tool[1:]))) + float(last_element)
+            _tool.clear()
             self.n_entry.delete(0,"end")
             self.n_entry.insert(0,result)
-        elif self.tool[0] == "mult":
+        elif _tool[0]== "mult":
             self.n_entry.get()
-            second_num = self.n_entry.get()
-            result = int(self.first_num) * int(second_num)
+            last_element = self.n_entry.get()
+            self.n_entry.delete(0, "end")
+            result = reduce(operator.mul,(list(map(float , _tool[1:])))) * float(last_element)
+            _tool.clear()
             self.n_entry.delete(0,"end")
             self.n_entry.insert(0,result)
 
-        elif self.tool[0] == "div":
+        elif _tool[0] == "div":
             self.n_entry.get()
-            second_num = self.n_entry.get()
-            result = int(self.first_num) / int(second_num)
+            last_element = self.n_entry.get()
+            self.n_entry.delete(0, "end")
+            result = reduce(operator.truediv,(list(map(float , _tool[1:])))) / float(last_element)
+            _tool.clear()
             self.n_entry.delete(0,"end")
             self.n_entry.insert(0,result)
-        elif self.tool[0] == "diff":
+        elif _tool[0] == "diff":
             self.n_entry.get()
-            second_num = self.n_entry.get()
-            result = int(self.first_num) - int(second_num)
+            last_element = self.n_entry.get()
+            self.n_entry.delete(0, "end")
+            result = reduce(operator.sub,(list(map(float , _tool[1:])))) - float(last_element)
+            _tool.clear()
             self.n_entry.delete(0,"end")
             self.n_entry.insert(0,result)
         elif self.tool[0] == "^":
